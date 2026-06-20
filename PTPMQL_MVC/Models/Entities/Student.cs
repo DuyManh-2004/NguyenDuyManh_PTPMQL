baee2a1ffaf90b9a04323b55f3898aaf7681f743
DuyManh-2004 <2221050029@student.humg.edu.vn>
2026-06-20 11:46:42 +0700

    Buoi7

--- PTPMQL_MVC/Models/Entities/Student.cs
+++ PTPMQL_MVC/Models/Entities/Student.cs
@@ -6,8 +6,11 @@ namespace PTPMQL_MVC.Models.Entities
 public class Student
 {
         [Key]
+        [MinLength(6, ErrorMessage = "Ma sinh vien phai co it nhat 6 ky tu")]
         public string StudentCode { get; set; } = default!;
+        [Required(ErrorMessage = "Ho va ten khong duoc de trong")]
         public string FullName { get; set; } = default!;
+        [Required(ErrorMessage = "Tuoi khong duoc de trong")]
         public int? Age { get; set; }
 }
 }
